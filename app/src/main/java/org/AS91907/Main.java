c47ba63230a0c3b7b2ef4f928d8b7dacf43df4a7
ChloeMayLikeCheese <cheeseidontlike4@gmail.com>
2026-05-19 15:02:22 +1200

    Initial commit

---
+++ app/src/main/java/org/AS91907/Main.java
@@ -0,0 +1,12 @@
+/*
+Author: Chloe T (https://github.com/ChloeMayLikeCheese)
+Purpose: Main class for an MP3 player
+Date: 19\05\2026
+*/
+package org.AS91907;
+
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
